3181b5b9013eb0740705e7cdce727faaea94d3fe
Blake Williams <blake@blakewilliams.me>
2014-01-07 12:35:35 -0600

    Add token to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, if: -> { name_changed? }
   validates :password, presence: true, if: -> { password.present? }
+
+  before_create :generate_token
+
+  private
+
+  def generate_token
+    begin
+      self.token = SecureRandom.hex
+    end while self.class.exists?(token: token)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,9 @@ describe User do
     user = FactoryGirl.build(:user)
     expect(user).to be_valid
   end
+
+  it 'generates a token on create' do
+    user = FactoryGirl.create(:user);
+    expect(user.token).not_to be_nil
+  end
 end
