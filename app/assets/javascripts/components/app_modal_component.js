0c474e197182270ee99cbdac6b3527d710cd6945
Blake Williams <blake@blakewilliams.me>
2014-01-18 13:49:57 -0600

    Make projects and projects new responsive

--- app/assets/javascripts/components/app_modal_component.js
+++ app/assets/javascripts/components/app_modal_component.js
@@ -1,4 +1,12 @@
 App.AppModalComponent = Ember.Component.extend({
+  hideOverflow: function() {
+    $('body').css({'overflow': 'hidden'});
+  }.on('didInsertElement'),
+
+  showOverflow: function() {
+    $('body').css({'overflow': 'scroll'});
+  }.on('willDestroyElement'),
+
   actions: {
     close: function() {
       this.sendAction('close');
