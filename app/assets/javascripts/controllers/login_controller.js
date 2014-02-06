39ad4f1d119dd4251a8d63831b7bba738e65bc37
Rob Levin <rob@milyoni.com>
2014-02-06 07:22:22 -0800

    As a user I'd like to be redirected to main page when I enter valid username and password. Tests for login success and fail

--- app/assets/javascripts/controllers/login_controller.js
+++ app/assets/javascripts/controllers/login_controller.js
@@ -2,8 +2,24 @@ App.LoginController = Ember.Controller.extend({
   email: null,
   password: null,
 
+  //Expose callbacks for better testability
+  callbacks: {
+    success: function(data) {
+      var router = this.get('target'),
+        token = data.token;
+      App.setToken(token);
+      router.transitionTo('projects');
+    },
+    fail: function(xhr, status, errThrown) {
+      //No-op
+      //TODO: Display error message(s). Holding off...need
+      //feedback from Blake on how we prefer validation UIUX to work
+      //console.log("Login failed...");
+    },
+  },
   actions: {
     login: function() {
+      var self = this;
       var session = $.ajax({
         url: App.get('apiPath') + '/sessions',
         type: 'POST',
@@ -12,10 +28,11 @@ App.LoginController = Ember.Controller.extend({
           password: this.get('password'),
         }
       });
-
       session.then(function(data) {
-        var token = data.token;
-        App.setToken(token);
+        self.callbacks.success.call(self, data);
+      });
+      session.fail(function(xhr, status, errThrown) {
+        self.callbacks.fail.call(self, xhr, status, errThrown);
       });
     }
   },
--- spec/javascripts/controllers/login_controller_spec.js
+++ spec/javascripts/controllers/login_controller_spec.js
@@ -1,20 +1,71 @@
 describe('App.LoginController', function() {
-  var server, controller;
+  var controller, route, response, failResponse, expectedToken;
   
   beforeEach(function() {
+    expectedToken = 'testtoken';
     controller = App.LoginController.create();
+    route = App.LoginRoute.create();
+    controller.set('target', route);
+    response =  {
+      then: function(cb) {
+        cb.call(this, {token: expectedToken});
+      },
+      fail: $.noop
+    };
+
+    failResponse =  {
+      then: $.noop,
+      fail: function(cb) {
+        cb.call(this, null, 'error', 'Unauthorized')
+      }
+    };
+  });
+
+  afterEach(function() {
+    controller = null;
+    route = null;
+    response = null;
   });
 
   it('sets app token on login', function() {
-    var response =  {
-      then: function(cb) {
-        cb.call(this, { token: 'testtoken' });
-      }
-    }
+    spyOn(route, 'transitionTo');
     spyOn($, 'ajax').and.returnValue(response);
 
     controller.send('login');
 
-    expect(App.get('token')).toBe('testtoken');
+    expect(App.get('token')).toBe(expectedToken);
   });
+
+  it('calls success helper on valid login', function() {
+    var callbacks = controller.get('callbacks');
+    spyOn(callbacks, "success");
+
+    spyOn($, 'ajax').and.returnValue(response);
+
+    controller.send('login');
+
+    expect(callbacks.success).toHaveBeenCalled();
+  });
+
+  it('redirects on login with valid login', function() {
+    var success = controller.get('callbacks').success;
+    var routerTransitionToSpy = spyOn(route, 'transitionTo');
+    var setTokenSpy = spyOn(App, 'setToken');
+    success.call(controller, {token: expectedToken});
+
+    expect(routerTransitionToSpy).toHaveBeenCalled();
+    expect(setTokenSpy).toHaveBeenCalledWith(expectedToken);
+  });
+
+  it('calls fail helper on login with invalid login', function() {
+    var callbacks = controller.get('callbacks');
+    spyOn(callbacks, "fail");
+
+    spyOn($, 'ajax').and.returnValue(failResponse);
+
+    controller.send('login');
+
+    expect(callbacks.fail).toHaveBeenCalled();
+  });
+
 });
